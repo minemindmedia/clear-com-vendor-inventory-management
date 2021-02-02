05a22f2de3d25ada6f3861d30625e0667f187ad2
ahmadnawaz121 <ahmadnawaz121@gmail.com>
2021-02-02 13:54:09 +0500

    sync data response fixed

--- assets/vendors.js
+++ assets/vendors.js
@@ -67,54 +67,32 @@ jQuery(document).ready(function ($) {
         dateFormat: 'yy-mm-dd'
     });
 
-    $('#sync-vendor-product-mapping').on('click', function (e) {
+    /* start sync vendor po and vendor product */
+    $('.sync-vendor-details').on('click', function (e) {
         e.preventDefault();
+        var sync_action = '';
+        if($(this).hasClass('sync-vendor-product-mapping')) {
+            sync_action = 'sync_vendor_product_mapping_table';
+        } else if($(this).hasClass('sync-vendor-po')) {
+            sync_action = 'sync_vendor_po_lookup_table';
+        } else if($(this).hasClass('update-vendor-po')) {
+            sync_action = 'update_vendor_po_lookup';
+        }
         $("body").append("<div id='wcvim_spinner' style='width: 100%;height: 100%;top: 0;left: 0;position: fixed;opacity: 0.7; background-color: #fff;z-index: 99;text-align: center;'><img style=' position: absolute;top: 50%;left: 50%;z-index:100 ' width='50' height='50' class='label-spinner' src='" + home_url + "'></div>");
         var data = {
-            action: 'sync_vendor_product_mapping_table',
+            action: sync_action,
         };
         var ajaxUrl = generate_po_ajax_object.ajax_url;
         jQuery.post(ajaxUrl, data, function (data) {
-            // var response = jQuery.parseJSON(data);
-            // console.log(response);
-            $('#wcvim_spinner').remove();
-            // if (response.success == true) {
-            //     window.location.reload();
-            // }
+            var response = jQuery.parseJSON(data);
+            // $('#wcvim_spinner').remove();
+            if (response.success == true) {
+                window.location.reload();
+            }
         });
     });
-    $('#sync-vendor-po').on('click', function (e) {
-        e.preventDefault();
-        $("body").append("<div id='wcvim_spinner' style='width: 100%;height: 100%;top: 0;left: 0;position: fixed;opacity: 0.7; background-color: #fff;z-index: 99;text-align: center;'><img style=' position: absolute;top: 50%;left: 50%;z-index:100 ' width='50' height='50' class='label-spinner' src='" + home_url + "'></div>");
-        var data = {
-            action: 'sync_vendor_po_lookup_table',
-        };
-        var ajaxUrl = generate_po_ajax_object.ajax_url;
-        jQuery.post(ajaxUrl, data, function (data) {
-            // var response = jQuery.parseJSON(data);
-            // console.log(response);
-            $('#wcvim_spinner').remove();
-            // if (response.success == true) {
-            //     window.location.reload();
-            // }
-        });
-    });
-    $('#update-vendor-po').on('click', function (e) {
-        e.preventDefault();
-        $("body").append("<div id='wcvim_spinner' style='width: 100%;height: 100%;top: 0;left: 0;position: fixed;opacity: 0.7; background-color: #fff;z-index: 99;text-align: center;'><img style=' position: absolute;top: 50%;left: 50%;z-index:100 ' width='50' height='50' class='label-spinner' src='" + home_url + "'></div>");
-        var data = {
-            action: 'update_vendor_po_lookup',
-        };
-        var ajaxUrl = generate_po_ajax_object.ajax_url;
-        jQuery.post(ajaxUrl, data, function (data) {
-            // var response = jQuery.parseJSON(data);
-            // console.log(response);
-            $('#wcvim_spinner').remove();
-            // if (response.success == true) {
-            //     window.location.reload();
-            // }
-        });
-    });
+    /* end sync vendor po and vendor product */
+
     $(document).on('click', '[data-role="order-title"]', function (event) {
         var element = $(this);
         var label = element.text();
